765535e56bae4ae5acf1f23e33f786ef3e48e413
duckbill70 <andrew.platts@hotmail.com>
2025-12-16 13:40:38 +0000

    Basic - Multi Device, Sanning Service Char subscription and reading from notifications in PeriperalSession.

---
+++ InjuryIQ - Xcode/ActivityType.swift
@@ -0,0 +1,8 @@
+//
+//  ActivityType.swift
+//  InjuryIQ - Xcode
+//
+//  Created by Platts Andrew on 16/12/2025.
+//
+
+import Foundation
---
+++ InjuryIQ - Xcode/PeripheralSession.swift
@@ -0,0 +1,8 @@
+//
+//  PeripheralSession.swift
+//  InjuryIQ - Xcode
+//
+//  Created by Platts Andrew on 16/12/2025.
+//
+
+import Foundation
