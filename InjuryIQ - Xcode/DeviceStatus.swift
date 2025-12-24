63292b10d08205b597f04793ec3dfe57078d7833
duckbill70 <andrew.platts@hotmail.com>
2025-12-24 16:47:23 +0000

    Rssi and hrz ti be added through ContentView

---
+++ InjuryIQ - Xcode/DeviceStatus.swift
@@ -0,0 +1,18 @@
+//
+//  DeviceStatus.swift
+//  InjuryIQ - Xcode
+//
+//  Created by Platts Andrew on 24/12/2025.
+//
+
+import SwiftUI
+
+struct DeviceStatus: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DeviceStatus()
+}
